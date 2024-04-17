4c9c19e1efc74bc7028114b40b167a8422fc063b
gtz <gtzafw@gmail.com>
2024-04-16 18:27:00 -0700

    build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 // dcos/.vuepress/config.js
 module.exports = {
   title: 'EECS221',
-  dest: './dist', 
+  dest: 'docs/.vuepress/dist', 
   base: '/EECS221/',
   repo: 
  'https://github.com/gtz123456/EECS221' // 添加 github 链接
--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
 # EECS221
 
-This is the project for EECS 221
+### This is the project for EECS 221
 Members: Mengting Yang, Tianzhou Gao, Xuzhong Chen, Ang Li
